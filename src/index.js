060c6820afafdb197ebf739b797f6fe072be1596
Oluwadara Olabampe <daraolabampe1@gmail.com>
2023-09-21 02:03:24 +0100

    mobile dnd

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+// import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <DndProvider backend={HTML5Backend}>
+  <DndProvider backend={TouchBackend}>
     <App />
   </DndProvider>
 );

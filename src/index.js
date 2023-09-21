669569b6b18a0dcd3752e57e4e7d6ffd41fc0df3
Oluwadara Olabampe <daraolabampe1@gmail.com>
2023-09-21 02:35:58 +0100

    dnd for phone

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-// import { TouchBackend } from "react-dnd-touch-backend";
+import {
+  MultiBackend,
+  TouchTransition,
+  MouseTransition,
+} from "react-dnd-multi-backend";
+import HTML5Backend from "react-dnd-html5-backend";
+import TouchBackend from "react-dnd-touch-backend";
 import "./index.css";
 import App from "./App";
 
+const HTML5toTouch = {
+  backends: [
+    {
+      backend: HTML5Backend,
+      transition: MouseTransition,
+    },
+    {
+      backend: TouchBackend,
+      transition: TouchTransition,
+    },
+  ],
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <DndProvider backend={HTML5Backend}>
+  <DndProvider backend={MultiBackend} options={HTML5toTouch}>
     <App />
   </DndProvider>
 );
